c4ad93669261f7fa06c462dbb2552cb6cb61e82b
RubenTobing122 <72769003+RubenTobing122@users.noreply.github.com>
2023-03-20 08:25:57 +0700

    Add files via upload

---
+++ storage/framework/views/140c958a79343bb7d2e76448627032160fa78247.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="style.css">
+    <title>my blog | home</title>
+</head>
+<body>
+        <h1> halaman utama </h1>
+</body>
+<script src="script.js"></script>
+</html>
+<?php /**PATH C:\Users\RubenElla\Documents\application\iseng-laravel\resources\views/home.blade.php ENDPATH**/ ?>
